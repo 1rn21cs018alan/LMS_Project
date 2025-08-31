b8d1704cbf2e625b9dcebd6c04a72b8d071d768b
Aneesh Jantikar <143446715+aneeshj-05@users.noreply.github.com>
2025-09-01 01:03:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ mongoose
 // --------- SERVER START ---------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(` Server running on port ${PORT}`);
 });
